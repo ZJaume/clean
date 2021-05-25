2c7029f6160e917e64fba992f25edd695b646e57
ZJaume <jzaragoza@prompsit.com>
2021-05-25 12:40:46 +0000

    DOGCV fixes

--- fixes/OPUS_DOGC_v2.sh
+++ fixes/OPUS_DOGC_v2.sh
@@ -11,6 +11,7 @@ temp=$(mktemp -d)
 tee >(cut -f1 | sacremoses -j 6 -l $SRC detokenize >$temp/$SRC.detok) \
     >(cut -f2 | sacremoses -j 6 -l $TRG detokenize >$temp/$TRG.detok)
 
+# join sides and detokenize dates an laws
 paste $temp/$SRC.detok $temp/$TRG.detok
 
 rm -r $temp
