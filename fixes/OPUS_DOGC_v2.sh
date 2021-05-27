05859c284b61a8bad99d0e9e2c8505f55901da44
ZJaume <jzaragoza@prompsit.com>
2021-05-27 14:58:53 +0000

    Fix detokenization that was still printing untokenized output, use a single detok script

--- fixes/OPUS_DOGC_v2.sh
+++ fixes/OPUS_DOGC_v2.sh
@@ -2,16 +2,4 @@
 set -e
 
 # Detokenize DOGC
-
-SRC=$1
-TRG=$2
-
-temp=$(mktemp -d)
-
-tee >(cut -f1 | sacremoses -j 6 -l $SRC detokenize >$temp/$SRC.detok) \
-    >(cut -f2 | sacremoses -j 6 -l $TRG detokenize >$temp/$TRG.detok)
-
-# join sides and detokenize dates an laws
-paste $temp/$SRC.detok $temp/$TRG.detok
-
-rm -r $temp
+fixes/detok.sh $1 $2
