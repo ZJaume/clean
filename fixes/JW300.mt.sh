c97985348052261d0a159d7ca93119df45561385
ZJaume <jzaragoza@prompsit.com>
2021-09-20 13:45:07 +0000

    Fix more detok issues in JW300 and Maltese

--- fixes/JW300.mt.sh
+++ fixes/JW300.mt.sh
@@ -2,4 +2,5 @@
 
 # Fix Maltese tokenization in JW300 that detokenizer cannot fix
 sed "s/ - $(echo -ne \u200b) /-/g" \
-    | sed 's/ - /-/g'
+    | sed 's/ - /-/g' \
+    | sed -E 's/(\w)- (\w)/\1-\2/g'
