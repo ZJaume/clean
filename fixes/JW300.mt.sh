0c3c6bab8bf173687ec0bba6908097ef7bc38db2
ZJaume <jzaragoza@prompsit.com>
2021-11-26 15:04:17 +0000

    Fix typo in JW300

--- fixes/JW300.mt.sh
+++ fixes/JW300.mt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Fix Maltese tokenization in JW300 that detokenizer cannot fix
-sed "s/ - $(echo -ne \u200b) /-/g" \
+sed "s/ - $(echo -ne "\u200b") /-/g" \
     | sed 's/ - /-/g' \
     | sed -E 's/(\w)- (\w)/\1-\2/g'
