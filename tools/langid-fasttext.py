a3da9e1ffec3b3c683304c4ac683ce686def9dbc
ZJaume <jzaragoza@prompsit.com>
2024-02-22 11:21:02 +0000

    Use openlid model

--- tools/langid-fasttext.py
+++ tools/langid-fasttext.py
@@ -14,9 +14,13 @@ import argparse
 import fasttext
 import os
 import sys
+from mtdata.iso.iso639_1 import ISO693_1_to_3
+ISO639_3to1 = {}
+for key, val in ISO693_1_to_3.items():
+    ISO639_3to1[val] = key
 
-BIN = "lid.176.bin"
-URL = "https://dl.fbaipublicfiles.com/fasttext/supervised-models/{}".format(BIN)
+BIN = "lid201-model.bin"
+URL = "https://data.statmt.org/lid/{}".format(BIN)
 
 fasttext.FastText.eprint = lambda x: None
 
@@ -26,15 +30,24 @@ def main():
     mpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), BIN)
     if not os.path.exists(mpath):
         sys.stderr.write("Downloading model {} ...\n".format(URL))
-        import urllib.request
-        urllib.request.urlretrieve(URL, mpath)
+        import gzip, shutil, urllib.request
+        with urllib.request.urlopen(URL + '.gz') as response:
+            with open(mpath, 'wb') as out_file, \
+                    gzip.GzipFile(fileobj=response) as decompressor:
+                shutil.copyfileobj(decompressor, out_file)
+
 
     model = fasttext.load_model(mpath)
 
     for line in sys.stdin:
         fields = line.strip().split("\t")
-        lid = model.predict(fields[args.field].lower())
-        sys.stdout.write("{}\t{}".format(lid[0][0][-2:], line))
+        lid = model.predict(fields[args.field].lower())[0][0][9:12]
+        print(lid)
+        if lid in ISO639_3to1:
+            lid = ISO639_3to1[lid]
+        else:
+            lid = 'unk'
+        sys.stdout.write("{}\t{}".format(lid, line))
 
 
 def parse_user_args():
